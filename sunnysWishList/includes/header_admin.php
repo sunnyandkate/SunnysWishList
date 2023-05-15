f591fb1be985ad1a40410d5beac876d8505c9f5e
kate <77579746+sunnyandkate@users.noreply.github.com>
2023-05-15 18:59:39 +0200

    Update header_admin.php

--- sunnysWishList/includes/header_admin.php
+++ sunnysWishList/includes/header_admin.php
@@ -14,10 +14,7 @@ if (!isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link rel="icon" href="images/favicon.ico">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Shadows+Into+Light+Two&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="styles2.css">
+     <link rel="stylesheet" type="text/css" href="styles2.css">
 </head>
 
 <body>
