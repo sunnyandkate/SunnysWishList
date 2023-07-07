d62b0afc0ba450db9f7504a7f75b26e071ef7251
kate <77579746+sunnyandkate@users.noreply.github.com>
2023-07-07 10:48:22 +0200

    Update db_connect.php

--- sunnysWishList/includes/db_connect.php
+++ sunnysWishList/includes/db_connect.php
@@ -3,8 +3,6 @@
 //connect to database
 $connection = mysqli_connect('localhost', 'root', '', 'sunnysWishList');
 
-//$connection = mysqli_connect('sdb-g.hosting.stackcp.net', 'sunnyswishlist-313835125b', 'montezuma*', 'sunnyswishlist-313835125b');
-
 
 //check connection
 if (!$connection) {
