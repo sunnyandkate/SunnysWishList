da6bbdfc4e74264bed8b56da00a29b6bd50e4148
kate <77579746+sunnyandkate@users.noreply.github.com>
2023-08-11 22:14:56 +0200

    Update footer.php

--- sunnysWishList/includes/footer.php
+++ sunnysWishList/includes/footer.php
@@ -1,6 +1,6 @@
         <footer>
             <div class="footer-left">
-                <p class="copyright">&copy; 2022 by sunnyandkate</p>
+                <p class="copyright">&copy; 2023 by sunnyandkate</p>
             </div>
             <p class="footer-center">sunnys birthday</p>
             <div class="footer-right">
@@ -10,25 +10,7 @@
 
         </footer>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
-        <script>
-            window.cookieconsent.initialise({
-                "palette": {
-                    "popup": {
-                        "background": "#4a6743"
-                    },
-                    "button": {
-                        "background": "#d6d099",
-                        "color": "#4a6743"
-                    }
-                },
-                "content": {
-                    "dismiss": "OK",
-                    "link": "learn more",
-                    "href": "http://sunnyandkate.com/privacypolicy.php"
-                }
-            });
-        </script>
+       <script defer src="cookieconsent-init.js"></script>
         </body>
 
 
