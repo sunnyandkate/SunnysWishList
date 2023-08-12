4dd3ef54d667a4ab5b6a6ff167d9f837b003d0d5
kate <77579746+sunnyandkate@users.noreply.github.com>
2023-08-12 16:56:20 +0200

    Update header.php

--- sunnysWishList/includes/header.php
+++ sunnysWishList/includes/header.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link rel="icon" href="images/favicon.ico">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v2.9.2/dist/cookieconsent.css">
-  <script src="https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v2.9.2/dist/cookieconsent.js"></script>
+    <link rel="stylesheet" href="cookieconsent.css">
+  <script src="cookieconsent.js"></script>
      <link rel="stylesheet" type="text/css" href="styles2.css">
 </head>
 
